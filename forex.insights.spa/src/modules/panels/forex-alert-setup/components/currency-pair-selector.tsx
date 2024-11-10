802059f004634633847d39b30233b598c78285f8
Manpreet Manni <manni.manpreet.developer@gmail.com>
2024-11-10 03:32:54 -0500

    fix styling issues

--- forex.insights.spa/src/modules/panels/forex-alert-setup/components/currency-pair-selector.tsx
+++ forex.insights.spa/src/modules/panels/forex-alert-setup/components/currency-pair-selector.tsx
@@ -1,7 +1,8 @@
 import { Component } from "react";
-import { Col, Form, Input, Row, Typography } from "antd";
+import { Col, Form, Input, Row, Select, Typography } from "antd";
 import { AlertItem } from "../enums";
 import styles from "./styles/currency-pair-selector.module.less";
+import { countries } from "./data";
 
 const { Item } = Form;
 const { Text } = Typography;
@@ -12,19 +13,36 @@ const { Text } = Typography;
 class CurrencyPairSelector extends Component {
     render() {
         return <Row>
-            <Col span={10}>
+            <Col span={24} md={10}>
                 <Item
                     name={AlertItem.toCurrency}
                 >
-                    <Input />
+                    <Select
+                        size="large"
+                        options={countries}
+                        optionRender={option =>
+                            <Row>
+                                <Col span={6}>
+                                    <img
+                                        className={styles.flag}
+                                        src={option.data.flag}
+                                        alt={option.data.name}
+                                    />
+                                </Col>
+                                <Col span={18}>
+                                    {option.data.currency + " - " + option.data.name}
+                                </Col>
+                            </Row>
+                        }
+                    />
                 </Item>
             </Col>
 
-            <Col span={4}>
+            <Col span={24} md={4}>
                 <Text className={styles.label}>to</Text>
             </Col>
 
-            <Col span={10}>
+            <Col span={24} md={10}>
                 <Item
                     name={AlertItem.fromCurrency}
                 >
