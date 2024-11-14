ec74d47dd12a37b9854ddb72224cd138a99bb80b
Manpreet Manni <manni.manpreet.developer@gmail.com>
2024-11-14 09:33:51 -0500

    updates

---
+++ forex.insights.api/Entities/ForexAlerts/ForexAlert.cs
@@ -0,0 +1,56 @@
+﻿using forex.insights.api.Entities.ForexAlerts.Enums;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace forex.insights.api.Entities.ForexAlerts
+{
+    /// <summary>
+    /// Forex Alert entity.
+    /// </summary>
+    public class ForexAlert
+    {
+        /// <summary>
+        /// Unique identifier.
+        /// </summary>
+        [Required]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Notification frequency.
+        /// </summary>
+        [Required]
+        public NotificationFrequency Frequency { get; set; }
+
+        /// <summary>
+        /// Currency to convert from.
+        /// </summary>
+        [Required]
+        [MaxLength(5)]
+        public string FromCurrency { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Currency to convert to.
+        /// </summary>
+        [Required]
+        [MaxLength(5)]
+        public string ToCurrency { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Minimum rate for alert.
+        /// </summary>
+        [Required]
+        [Precision(6, 2)]
+        public decimal MinimumRate { get; set; }
+
+        /// <summary>
+        /// Contact method.
+        /// </summary>
+        [Required]
+        public ContactMethod ContactMethod { get; set; }
+
+        /// <summary>
+        /// Next alert time.
+        /// </summary>
+        public string? NextAlertTime { get; set; }
+    }
+}
---
+++ forex.insights.api/Program.cs
@@ -0,0 +1,24 @@
+namespace forex.insights.api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.Run();
+        }
+    }
+}
