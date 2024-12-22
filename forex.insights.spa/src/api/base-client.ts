7f805d25645d80f773578be676fb9171be5da0ad
Manpreet Manni <manni.manpreet.developer@gmail.com>
2024-12-21 19:48:15 -0500

    updates

--- forex.insights.spa/src/api/base-client.ts
+++ forex.insights.spa/src/api/base-client.ts
@@ -14,16 +14,16 @@ class BaseClient {
      * Constructor for the base client.
      */
     constructor() {
-        this.baseUrl = import.meta.env.VITE_BASE_API_URL ?? "";
+        this.baseUrl = import.meta.env.VITE_BASE_API_URL;
     }
 
     /**
      * Fetch data from the API.
-     * @param endpoint Endpoint to fetch data from.
      * @param method HTTP method to use.
      * @param requestData Request data to send.
+     * @param endpoint Endpoint to fetch data from.
      */
-    protected async fetchData<T>(endpoint: string, method: HttpMethod, requestData: object): Promise<T> {
+    protected async fetchData<T>(method: HttpMethod, requestData: object, endpoint: string): Promise<T> {
         // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
         return await fetch(`${this.baseUrl}/${endpoint}`, {
             method,
--- forex.insights.spa/src/api/forex-alert/forex-alert-client.ts
+++ forex.insights.spa/src/api/forex-alert/forex-alert-client.ts
@@ -1,6 +1,6 @@
 import BaseClient from "../base-client";
 import { HttpMethod } from "../enums";
-import { ForexAlertGetRequest, ForexAlertGetResponse } from "./interfaces";
+import { ForexAlertGetRequest, ForexAlertGetResponse, ForexAlertPatchRequest, ForexAlertPostRequest, ForexAlertPostResponse, ForexAlertSearchRequest, ForexAlertSearchResponse } from "./interfaces";
 
 /**
  * Api client for forex alert.
@@ -13,7 +13,59 @@ class ForexAlertClient extends BaseClient {
      * @param data Request data.
      */
     get(data: ForexAlertGetRequest) {
-        return this.fetchData<ForexAlertGetResponse>(`${this.endpoint}/${data.id}`, HttpMethod.Get, data);
+        return this.fetchData<ForexAlertGetResponse>(
+            HttpMethod.Get,
+            data,
+            `${this.endpoint}/${data.id}`
+        );
+    }
+
+    /**
+     * Search forex alerts.
+     * @param data Request data.
+     */
+    search(data: ForexAlertSearchRequest) {
+        return this.fetchData<ForexAlertSearchResponse>(
+            HttpMethod.Get,
+            data,
+            `${this.endpoint}?${data.ids.length > 0 ? data.ids.map(id => `ids=${id}&`) : ""}`
+        );
+    }
+
+    /**
+     * Create a new forex alert.
+     * @param data Request data.
+     */
+    post(data: ForexAlertPostRequest) {
+        return this.fetchData<ForexAlertPostResponse>(
+            HttpMethod.Post,
+            data,
+            `${this.endpoint}`
+        );
+    }
+
+    /**
+     * Update an existing forex alert.
+     * @param data Request data.
+     */
+    patch(data: ForexAlertPatchRequest) {
+        return this.fetchData<void>(
+            HttpMethod.Patch,
+            data,
+            `${this.endpoint}/${data.id}`
+        );
+    }
+
+    /**
+     * Delete an existing forex alert.
+     * @param data Request data.
+     */
+    delete(data: ForexAlertGetRequest) {
+        return this.fetchData<void>(
+            HttpMethod.Delete,
+            data,
+            `${this.endpoint}/${data.id}`
+        );
     }
 }
 
