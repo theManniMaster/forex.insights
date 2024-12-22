bc0113a2c5609ca7d3bc71b4e35bfa29fdb72dee
Manpreet Manni <manni.manpreet.developer@gmail.com>
2024-12-21 19:41:56 -0500

    updates

--- forex.insights.api/DataModels/ForexAlerts/ForexAlertSearchRequest.cs
+++ forex.insights.api/DataModels/ForexAlerts/ForexAlertSearchRequest.cs
@@ -8,6 +8,17 @@
         /// <summary>
         /// Forex Alert Ids.
         /// </summary>
-        public IEnumerable<Guid> Ids { get; set; } = [];
+        public IEnumerable<Guid> Ids { get; set; } = new List<Guid>();
+
+        /*
+         * Developer Note:
+         * 
+         * Dont assign [] to Ids as the compiler is suggesting.
+         * .NET had issues with binding to Ids, even if the correct url is provided.
+         * 
+         * This is because assigning [] to a variable means assigning Array.Empty<Guid>() to it.
+         * Since arrays are immutable, the Ids property will always be an empty array,
+         * as .NET will not be able to add values to it.
+         */
     }
 }
