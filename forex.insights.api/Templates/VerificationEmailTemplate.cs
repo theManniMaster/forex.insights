03673f04ca4cecce26c683715aa7f4e9e5d30e42
Manpreet Manni <manni.manpreet.developer@gmail.com>
2024-12-31 15:27:00 -0500

    updates

---
+++ forex.insights.api/Templates/VerificationEmailTemplate.cs
@@ -0,0 +1,40 @@
+﻿using forex.insights.api.Entities.ForexAlerts;
+using SendGrid.Helpers.Mail;
+
+namespace forex.insights.api.Templates
+{
+    /// <summary>
+    /// Verification Email Template.
+    /// </summary>
+    /// <param name="fromEmail">Sender email.</param>
+    /// <param name="toEmail">Receiver email.</param>
+    /// <param name="subject">Email subject.</param>
+    /// <param name="content">Email content.</param>
+    public class VerificationEmailTemplate(string fromEmail, string toEmail, string subject, string content)
+    {
+        /// <summary>
+        /// Create Email message.
+        /// </summary>
+        /// <returns>Message instance.</returns>
+        public SendGridMessage CreateMessage()
+        {
+            return MailHelper.CreateSingleEmail(
+                new EmailAddress(fromEmail),
+                new EmailAddress(toEmail),
+                subject,
+                GetContent(),
+                GetContent()
+            );
+        }
+
+        /// <summary>
+        /// Get the content for the email.
+        /// </summary>
+        /// <returns>Html Content.</returns>
+        private string GetContent()
+        {
+            return $"<!doctype html><html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:v=\"urn:schemas-microsoft-com:vml\" xmlns:o=\"urn:schemas-microsoft-com:office:office\"><head><title></title><!--[if !mso]><!--><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"><!--<![endif]--><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"><meta name=\"viewport\" content=\"width=device-width,initial-scale=1\"><style type=\"text/css\">#outlook a{{padding:0}}body{{margin:0;padding:0;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%}}table,td{{border-collapse:collapse;mso-table-lspace:0;mso-table-rspace:0}}img{{border:0;height:auto;line-height:100%;outline:0;text-decoration:none;-ms-interpolation-mode:bicubic}}p{{display:block;margin:13px 0}}</style><!--[if mso]><noscript><xml><o:officedocumentsettings><o:allowpng><o:pixelsperinch>96</o:pixelsperinch></o:officedocumentsettings></xml></noscript><![endif]--><!--[if lte mso 11]><style type=\"text/css\">.mj-outlook-group-fix{{width:100%!important}}</style><![endif]--><style type=\"text/css\">@media only screen and (min-width:480px){{.mj-column-per-100{{width:100%!important;max-width:100%}}.mj-column-per-50{{width:50%!important;max-width:50%}}</style><style media=\"screen and (min-width:480px)\">.moz-text-html .mj-column-per-100{{width:100%!important;max-width:100%}}.moz-text-html .mj-column-per-50{{width:50%!important;max-width:50%}}</style><style type=\"text/css\"></style></head><body style=\"word-spacing:normal;background-color:#f5f5f7\"><div style=\"background-color:#f5f5f7\"><!--[if mso | IE]><table align=\"center\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" class=\"\" style=\"width:600px\" width=\"600\"><tr><td style=\"line-height:0;font-size:0;mso-line-height-rule:exactly\"><![endif]--><div style=\"margin:0 auto;max-width:600px\"><table align=\"center\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" role=\"presentation\" style=\"width:100%\"><tbody><tr><td style=\"direction:ltr;font-size:0;padding:16px 4px;text-align:center\"><!--[if mso | IE]><table role=\"presentation\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\"><tr><td class=\"\" width=\"600px\"><table align=\"center\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" class=\"\" style=\"width:592px\" width=\"592\" bgcolor=\"white\"><tr><td style=\"line-height:0;font-size:0;mso-line-height-rule:exactly\"><![endif]--><div style=\"background:#fff;background-color:#fff;margin:0 auto;border-radius:24px;max-width:592px\"><table align=\"center\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" role=\"presentation\" style=\"background:#fff;background-color:#fff;width:100%;border-radius:24px\"><tbody><tr><td style=\"direction:ltr;font-size:0;padding:20px 0;text-align:center\"><!--[if mso | IE]><table role=\"presentation\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\"><tr><td class=\"\" style=\"vertical-align:top;width:592px\"><![endif]--><div class=\"mj-column-per-100 mj-outlook-group-fix\" style=\"font-size:0;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%\"><table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" role=\"presentation\" width=\"100%\"><tbody><tr><td style=\"vertical-align:top;padding:8px\"><table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" role=\"presentation\" width=\"100%\"><tbody><tr><td align=\"left\" style=\"font-size:0;padding:10px 25px;word-break:break-word\"><div style=\"font-family:Arial;font-size:28px;line-height:1;text-align:left;color:#000\">Forex Insights</div></td></tr><tr><td align=\"left\" style=\"font-size:0;padding:10px 25px;padding-top:8px;word-break:break-word\"><div style=\"font-family:Arial;font-size:14px;letter-spacing:.5px;line-height:1.5;text-align:left;color:#6e6e73\">{content}</div></td></tr></tbody></table></td></tr></tbody></table></div><!--[if mso | IE]><td class=\"\" style=\"vertical-align:top;width:592px\"><![endif]--><div class=\"mj-column-per-100 mj-outlook-group-fix\" style=\"font-size:0;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%\"><table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" role=\"presentation\" width=\"100%\"><tbody><tr><td style=\"vertical-align:top;padding:8px\"><table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" role=\"presentation\" width=\"100%\"><tbody><tr><td align=\"left\" style=\"font-size:0;padding:10px 25px;word-break:break-word\"><div style=\"font-family:Arial;font-size:14px;letter-spacing:.5px;line-height:1.5;text-align:left;color:#6e6e73\">If you’d like to adjust your preferences, please log into your account.</div></td></tr></tbody></table></td></tr></tbody></table></div><!--[if mso | IE]><td class=\"\" style=\"vertical-align:top;width:592px\"><![endif]--><div class=\"mj-column-per-100 mj-outlook-group-fix\" style=\"font-size:0;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%\"><table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" role=\"presentation\" width=\"100%\"><tbody><tr><td style=\"vertical-align:top;padding:8px\"><table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" role=\"presentation\" width=\"100%\"><tbody><tr><td align=\"left\" style=\"font-size:0;padding:10px 25px;word-break:break-word\"><div style=\"font-family:Arial;font-size:16px;font-weight:600;line-height:1;text-align:left;color:#000\">Just a Developer.</div></td></tr><tr><td align=\"left\" style=\"font-size:0;padding:10px 25px;word-break:break-word\"><div style=\"font-family:Arial;font-size:13px;letter-spacing:.5px;line-height:1;text-align:left;color:#6e6e73\">www.forexinsights.com</div></td></tr></tbody></table></td></tr></tbody></table></div><!--[if mso | IE]><![endif]--></td></tr></tbody></table></div><!--[if mso | IE]><![endif]--></td></tr></tbody></table></div><!--[if mso | IE]><![endif]--></div></body></html>";
+        }
+    }
+}
+
