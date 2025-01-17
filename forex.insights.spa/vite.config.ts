e770761c52df038f5557ba960500d4ee536f0e4c
Manpreet Manni <manni.manpreet.developer@gmail.com>
2025-01-16 23:21:57 -0500

    updates

--- forex.insights.spa/vite.config.ts
+++ forex.insights.spa/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/forex.insights/"
+    plugins: [react()],
+    base: "/forex.insights/",
+    build: {
+        chunkSizeWarningLimit: 1600
+    }
 })
