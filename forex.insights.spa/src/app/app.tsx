a3f7d990a771b5635f2b8427858ad0f8f49a7028
Manpreet Manni <manni.manpreet.developer@gmail.com>
2025-01-18 16:38:32 -0500

    cleanup

--- forex.insights.spa/src/app/app.tsx
+++ forex.insights.spa/src/app/app.tsx
@@ -2,7 +2,7 @@ import { Component } from "react";
 import { RouterProvider } from "react-router-dom";
 import { ConfigProvider } from "antd";
 import { theme } from "./themes";
-import { router, Routes } from "../router";
+import { router } from "../router";
 
 import "./configs";
 
@@ -10,27 +10,6 @@ import "./configs";
  * Main application component.
  */
 class App extends Component {
-
-    componentDidMount = () => {
-        /**
-         * Developer Note:
-         * 
-         * This code is a workaround for an issue with GitHub Pages and not directly related to the application.
-         * 
-         * The application is hosted on GitHub Pages at the URL `url/forex.insights/`, and pages like `url/forex.insights/login` are accessed by navigation.
-         * When a user refreshes a page, GitHub Pages tries to reload the page at the same URL (e.g., `url/forex.insights/login`), but since GitHub Pages only
-         * serves static content from the root URL, it results in a 404 error.
-         * 
-         * This code detects when the page is reloaded and redirects the user back to the root URL to prevent the 404 error. And react router handles the rest.
-         */
-
-        const navigationEntries = window.performance.getEntriesByType("navigation") as PerformanceNavigationTiming[];
-
-        if (navigationEntries.length > 0 && navigationEntries[0].type === "reload") {
-            window.location.replace(Routes.root);
-        }
-    }
-
     render() {
         return <ConfigProvider theme={theme}>
             <RouterProvider router={router} />
