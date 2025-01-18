c8948049f06f8118c360aab0fc42aca37c82c60f
Manpreet Manni <manni.manpreet.developer@gmail.com>
2025-01-18 16:01:33 -0500

    updates

--- forex.insights.spa/src/router/enum/routes.ts
+++ forex.insights.spa/src/router/enum/routes.ts
@@ -1,17 +1,17 @@
-const base = "/forex.insights";
+const base = "/forex.insights/";
 
 /**
  * Enum for routes.
  */
 enum Routes {
     root = base,
-    login = `${base}/login`,
-    register = `${base}/register`,
-    dashboard = `${base}/dashboard`,
-    create = `${base}/create`,
-    edit = `${base}/edit`,
-    feedback = `${base}/feedback`,
-    oops = `${base}/oops`,
+    login = `${base}login`,
+    register = `${base}register`,
+    dashboard = `${base}dashboard`,
+    create = `${base}create`,
+    edit = `${base}edit`,
+    feedback = `${base}feedback`,
+    oops = `${base}oops`,
 }
 
 export default Routes;
